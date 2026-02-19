ef4cdf790bdb988913f81cfa8522ebacb7235f86
ashirwadexe <ashirwadbappy@gmail.com>
2026-02-19 09:31:50 +0530

    backend deploy setup-v1

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
